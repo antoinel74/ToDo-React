cc59f3beccb18b68b9c92a841f04ecf23e4acce2
antoinel74 <antoine.lansman@gmail.com>
2023-11-27 09:59:40 +0100

    up

---
+++ src/components/Todolist.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+let Todolist = ({ todos, checkboxChange }) => {
+  const taskStyle = {
+    textDecoration: "line-through",
+    opacity: 0.4,
+  };
+
+  const nodeRef = useRef(null);
+
+  return (
+    <ul className="tasks">
+      <TransitionGroup component={null}>
+        {todos.map((todo, index) => (
+          <CSSTransition
+            key={index}
+            classNames="fade"
+            timeout={330}
+            nodeRef={nodeRef}
+          >
+            <li className="task fade" key={index} ref={nodeRef}>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => checkboxChange(index)}
+                />{" "}
+                <span style={todo.completed ? taskStyle : null}>
+                  {todo.task}
+                </span>
+              </div>
+              <span style={todo.completed ? taskStyle : null}>
+                {todo.dateTo}
+              </span>
+            </li>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </ul>
+  );
+};
+
+export default Todolist;
